5a17a63391bfd7828b86381c061e84b073bd1289
Bruno Hoffmann <bruno.siqueira.hoffmann@gmail.com>
2022-08-08 09:45:13 -0300

    feat: adding structure

--- forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,52 +1,40 @@
 package br.com.alura.forum.controller.form;
 
-import br.com.alura.forum.modelo.Curso;
-import br.com.alura.forum.modelo.Topico;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.Length;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
+
 public class TopicoForm {
-    @NotNull @NotEmpty
-    private String titulo;
 
-    @NotNull @NotEmpty
-    private String mensagem;
+	@NotNull @NotEmpty @Length(min = 5)
+	private String titulo;
+	
+	@NotNull @NotEmpty @Length(min = 10)
+	private String mensagem;
+	
+	@NotNull @NotEmpty
+	private String nomeCurso;
 
-    @NotNull @NotEmpty
-    private String nomeCurso;
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
 
-    public TopicoForm(String titulo, String mensagem, String nomeCurso) {
-        this.titulo = titulo;
-        this.mensagem = mensagem;
-        this.nomeCurso = nomeCurso;
-    }
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 
-    public String getTitulo() {
-        return titulo;
-    }
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+	public Topico converter(CursoRepository cursoRepository) {
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		return new Topico(titulo, mensagem, curso);
+	}
 
-    public String getMensagem() {
-        return mensagem;
-    }
-
-    public void setMensagem(String mensagem) {
-        this.mensagem = mensagem;
-    }
-
-    public String getNomeCurso() {
-        return nomeCurso;
-    }
-
-    public void setNomeCurso(String nomeCurso) {
-        this.nomeCurso = nomeCurso;
-    }
-
-    public Topico converter(Curso curso) {
-        return new Topico(this.titulo, this.mensagem, curso);
-    }
 }
--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$12$fIaiJArPFX2H.z6WrH07U.Pj5hKFBSXQsVHZF8lZtFGvPQnH/ojMK');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
